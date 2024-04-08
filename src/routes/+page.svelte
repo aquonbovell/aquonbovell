049bfe325713a556579eb7912622b784d5e8a940
Aquon Bovell <aquon_bovell@outlook.com>
2024-04-07 22:13:55 -0400

    Update project configurations and add new files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
+<script>
+	import Main from '../components/Main.svelte';
+</script>
 
 <svelte:head>
-  <title>Aquon Bovell Portfolio</title>
-  <meta name="portfolio" content="portfolio web development" />
+	<title>Aquon Bovell Portfolio</title>
+	<meta name="portfolio" content="portfolio web development" />
 </svelte:head>
-<script>
-  import Main from "../components/Main.svelte";
-</script>
 
 <Main />
